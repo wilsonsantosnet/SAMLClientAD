1bc3ac66a8811e4512b211c69fef60da92ad5c70
Wilson dos Santos <wilsonsantosnet@gmail.com>
2023-01-19 15:23:36 -0300

    initial

---
+++ RsaPkCs1Sha1SignatureDescription.cs
@@ -0,0 +1,20 @@
+﻿namespace SAMLClientAD
+{
+    public class RsaPkCs1Sha1SignatureDescription : System.Security.Cryptography.SignatureDescription
+    {
+        public RsaPkCs1Sha1SignatureDescription()
+        {
+            KeyAlgorithm = "System.Security.Cryptography.RSACryptoServiceProvider";
+            DigestAlgorithm = "System.Security.Cryptography.SHA1Managed";
+            FormatterAlgorithm = "System.Security.Cryptography.RSAPKCS1SignatureFormatter";
+            DeformatterAlgorithm = "System.Security.Cryptography.RSAPKCS1SignatureDeformatter";
+        }
+        public override System.Security.Cryptography.AsymmetricSignatureDeformatter CreateDeformatter(System.Security.Cryptography.AsymmetricAlgorithm key)
+        {
+            var asymmetricSignatureDeformatter = (System.Security.Cryptography.AsymmetricSignatureDeformatter)System.Security.Cryptography.CryptoConfig.CreateFromName(DeformatterAlgorithm);
+            asymmetricSignatureDeformatter.SetKey(key);
+            asymmetricSignatureDeformatter.SetHashAlgorithm("SHA1");
+            return asymmetricSignatureDeformatter;
+        }
+    }
+}
---
+++ RsaPkCs1Sha256SignatureDescription.cs
@@ -0,0 +1,20 @@
+﻿namespace SAMLClientAD
+{
+    public class RsaPkCs1Sha256SignatureDescription : System.Security.Cryptography.SignatureDescription
+    {
+        public RsaPkCs1Sha256SignatureDescription()
+        {
+            KeyAlgorithm = "System.Security.Cryptography.RSACryptoServiceProvider";
+            DigestAlgorithm = "System.Security.Cryptography.SHA256Managed";
+            FormatterAlgorithm = "System.Security.Cryptography.RSAPKCS1SignatureFormatter";
+            DeformatterAlgorithm = "System.Security.Cryptography.RSAPKCS1SignatureDeformatter";
+        }
+        public override System.Security.Cryptography.AsymmetricSignatureDeformatter CreateDeformatter(System.Security.Cryptography.AsymmetricAlgorithm key)
+        {
+            var asymmetricSignatureDeformatter = (System.Security.Cryptography.AsymmetricSignatureDeformatter)System.Security.Cryptography.CryptoConfig.CreateFromName(DeformatterAlgorithm);
+            asymmetricSignatureDeformatter.SetKey(key);
+            asymmetricSignatureDeformatter.SetHashAlgorithm("SHA256");
+            return asymmetricSignatureDeformatter;
+        }
+    }
+}
